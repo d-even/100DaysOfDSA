431ddbcf431d0c99570dc193f257ab4a87ba934e
Deven <140685852+d-even@users.noreply.github.com>
2026-04-23 22:07:12 +0530

    Number Based added

---
+++ NumberBased/TwoSum.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+public class TwoSum{
+    public static void main(String[] args) {
+        
+        int[] arr = {2,3,5,7,1};
+        int target = 8;
+
+        int[] ans = twoSum(arr, target);
+        if (ans.length == 2) {
+            System.out.println("Indices: [" + ans[0] + ", " + ans[1] + "]");
+        } else {
+            System.out.println("No pair found");
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (map.containsKey(diff)) {
+                return new int[] { map.get(diff), i };
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+}
